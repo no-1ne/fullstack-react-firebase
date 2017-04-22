9876e66b4b7f0a8b73f0f3c4443f11ab2a499418
No One <no1@webgurukul.io>
2017-04-22 00:18:15 +0000

    Firebase+Connecting-App-Backend

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ import "antd/dist/antd.css";
 
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
 
-
+const config = {
+    apiKey: "AIzaSyBYAQS6PX9gGAIt43Tf6yUTr_PEPUd35yk",
+    authDomain: "mycommerce-6038a.firebaseapp.com",
+    databaseURL: "https://mycommerce-6038a.firebaseio.com",
+    projectId: "mycommerce-6038a",
+    storageBucket: "mycommerce-6038a.appspot.com",
+    messagingSenderId: "7864811118"
+  };
+firebase.initializeApp(config);
+  
 ReactDOM.render(
   <App />,
   document.getElementById('root')
