13de3326b278c7279d53792a8a2e8a240c46d822
No One <no1@webgurukul.io>
2017-04-27 00:17:19 +0000

    Form-Get-User-Input

--- src/NewItem.js
+++ src/NewItem.js
@@ -1,12 +1,37 @@
 /*eslint-disable*/
 import React, { Component } from 'react';
-
+import {Input, InputNumber, Button} from 'antd';
 class NewItem extends Component {
+
+
+ handleSubmit(event){
+    event.preventDefault();
+    console.log(event.target.title.value);
+    console.log(event.target.description.value);
+    console.log(event.target.image.value);
+    console.log(event.target.price.value);
+    console.log(event.target.discount.value);
+ }
+
 
  render() {
     return (
     <div> 
-        Adding New Item goes here 
+    <h3>Add an Item to be Sold</h3>
+    <hr/><br/>
+      <form onSubmit={this.handleSubmit}>
+        <Input placeholder="Item Title" name='title' required/>
+
+        <Input placeholder="Image" name='image'required />
+        <Input type="textarea" rows={4}
+            placeholder="Description" name='description' required/>
+        
+        <span>Title: </span><InputNumber placeholder="Price" name='price' defaultValue={100}/>    
+        <span>Discount: </span><InputNumber placeholder="Discount" name='discount' defaultValue={0}/>  
+        <br/><br/>
+        <Button type="primary" htmlType='submit' >Add Item</Button>
+      </form>
+      
     </div>
     );
      
