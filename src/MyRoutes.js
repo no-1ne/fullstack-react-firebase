bcf487f978e6a6e8e8f6f5e8fbf136bb00b73c0f
No One <no1@webgurukul.io>
2017-04-28 12:25:33 +0000

    FB-Google-signin

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,30 +1,82 @@
 /*eslint-disable*/
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import { Layout, Menu, Icon, Modal, Button} from 'antd';
+import { Layout, Menu, Icon, Modal, Button, notification} from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 
 import App from './App.js';
 import NewItem from './NewItem.js';
+
+import * as firebase from 'firebase';
 
 class MyRoutes extends Component {
 
 constructor(props) {
     super(props);
     this.state = {
-        visible : false
+        visible : false,
+        loggedIn: false
     };
     this.handleSiderMenuClick = this.handleSiderMenuClick.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleAuthChange = this.handleAuthChange.bind(this);
 }
 
+handleGoogleLogin(){
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  
+}
+
+handleFacebookLogin() {
+    let provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+      
+}
+
+authRedirectSuccess(result) {
+    console.log(result);
+    if(result.user != null ) {
+        notification.success({
+        message: 'Welcome'+result.user.displayName,
+        description: 'Have a great Shopping experince'
+      });
+  }
+
+}
+
+authRedirectFail(reason) {
+    console.log(reason);
+}
+
+handleAuthChange(user) {
+    if(user !==null) {
+        this.setState({
+            loggedIn: true
+        });
+    }
+}
+
+componentDidMount (){
+    firebase.auth().getRedirectResult().then(this.authRedirectSuccess).catch(this.authRedirectFail);
+    firebase.auth().onAuthStateChanged(this.handleAuthChange);
+}
 
 handleSiderMenuClick(propsPassed) {
     console.log(propsPassed);
     if(propsPassed.key == 3){
+        
+        if(this.state.loggedIn != true) {
         this.setState({
             visible: true
         });
+        } else {
+            firebase.auth().signOut();
+            this.setState({
+            loggedIn: false
+        });
+        }
+
     }
 }
 
@@ -35,6 +87,10 @@ handleCancel() {
 }
 
  render() {
+    let signInText = 'SignIn/Register'; 
+    if(this.state.loggedIn == true) {
+        signInText = 'Logout'
+    }
     return (<BrowserRouter>
         <div>
         
@@ -57,7 +113,7 @@ handleCancel() {
     <Menu.Item key="3">
         <Icon type="key" />
         <span className="nav-text">
-          SignIn/Register
+         {signInText}
             
          </span>
       </Menu.Item>
@@ -87,12 +143,16 @@ handleCancel() {
     </Layout>
     <Modal title="Login" visible={this.state.visible}
            onCancel={this.handleCancel}
-          footer={null} width='300'
+          footer={null} width='300px'
         >
          
-         <Button type="primary" style={{width: '100%', background: 'red'}}>Login with Google</Button>
+         <Button type="primary" onClick={this.handleGoogleLogin}
+            style={{width: '100%', background: 'red'}}>
+            Login with Google</Button>
          <br/><br/>
-         <Button type="primary" style={{width: '100%', background: 'blue'}}>Login with Facebook</Button>
+         <Button type="primary" onClick={this.handleFacebookLogin}
+            style={{width: '100%', background: 'blue'}}>
+            Login with Facebook</Button>
         </Modal>   
         </div>
         
