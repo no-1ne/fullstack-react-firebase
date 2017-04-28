a6cb7f4dff7ba31416caed669ce40c87dc9dfb05
No One <no1@webgurukul.io>
2017-04-28 10:49:02 +0000

    Login-Modal-OnClick

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,13 +1,38 @@
 /*eslint-disable*/
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import { Layout, Menu, Icon} from 'antd';
+import { Layout, Menu, Icon, Modal, Button} from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 
 import App from './App.js';
 import NewItem from './NewItem.js';
 
 class MyRoutes extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+        visible : false
+    };
+    this.handleSiderMenuClick = this.handleSiderMenuClick.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+}
+
+
+handleSiderMenuClick(propsPassed) {
+    console.log(propsPassed);
+    if(propsPassed.key == 3){
+        this.setState({
+            visible: true
+        });
+    }
+}
+
+handleCancel() {
+  this.setState({
+    visible: false
+  });  
+}
 
  render() {
     return (<BrowserRouter>
@@ -16,7 +41,8 @@ class MyRoutes extends Component {
             <Layout >
       <Sider breakpoint="md" collapsedWidth="0" style={{ background: '#fff' }}>
       
-    <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} style={{marginTop: '64px'}}>
+    <Menu theme="light" mode="inline" onClick={this.handleSiderMenuClick}
+        defaultSelectedKeys={['1']} style={{marginTop: '64px'}}>
       <Menu.Item key="1" disabled>
         <Icon type="user" />
         <span className="nav-text">My Account</span>
@@ -25,6 +51,13 @@ class MyRoutes extends Component {
         <Icon type="to-top" />
         <span className="nav-text">
          <Link to='/new-item'>   Upload Item </Link>
+            
+         </span>
+      </Menu.Item>
+    <Menu.Item key="3">
+        <Icon type="key" />
+        <span className="nav-text">
+          SignIn/Register
             
          </span>
       </Menu.Item>
@@ -52,8 +85,19 @@ class MyRoutes extends Component {
         <Footer>Footer</Footer>
       </Layout>
     </Layout>
-     
+    <Modal title="Login" visible={this.state.visible}
+           onCancel={this.handleCancel}
+          footer={null} width='300'
+        >
+         
+         <Button type="primary" style={{width: '100%', background: 'red'}}>Login with Google</Button>
+         <br/><br/>
+         <Button type="primary" style={{width: '100%', background: 'blue'}}>Login with Facebook</Button>
+        </Modal>   
         </div>
+        
+        
+        
      </BrowserRouter>
      );
      
